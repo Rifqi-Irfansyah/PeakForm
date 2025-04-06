bdc68b7ae0605d65ae93e05eed5c08fce2ad95ff
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2025-04-06 15:59:01 +0700

    feat: complete make schedule in every conditions

--- app/src/main/java/com/example/peakform/ViewModel/VMMakeSchedule.kt
+++ app/src/main/java/com/example/peakform/ViewModel/VMMakeSchedule.kt
@@ -9,6 +9,8 @@ import com.example.peakform.API.ApiService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import okhttp3.RequestBody
+import okhttp3.Response
 
 class VMMakeSchedule : ViewModel() {
     private val _loading = MutableStateFlow(false)
@@ -19,23 +21,34 @@ class VMMakeSchedule : ViewModel() {
 
     private val _error = MutableStateFlow<String?>(null)
     val error: StateFlow<String?> = _error
-
+    var allSuccess = true
 
     fun makeSchedule(answers: Map<Int, String>) {
-        val idExerciseType = mutableMapOf<String, Any>()
-        idExerciseType["ChestBeginner"] = 1
-        idExerciseType["ChestIntermediate"] = 2
+        val idExerciseType = mutableMapOf<String, List<Int>>()
+        idExerciseType["ChestEasy"]     = listOf(125,165,166)
+        idExerciseType["ChestHard"]     = listOf(167,168,169)
+        idExerciseType["TricepsEasy"]   = listOf(144,145)
+        idExerciseType["TricepsHard"]   = listOf(146,147)
+        idExerciseType["BackEasy"]      = listOf(129,130,133,136)
+        idExerciseType["BackHard"]      = listOf(129,130,134,137)
+        idExerciseType["Shoulder"]      = listOf(170,171,172,173)
+        idExerciseType["Traps"]         = listOf(142,143)
+        idExerciseType["Neck"]          = listOf(138)
+        idExerciseType["BicepsEasy"]    = listOf(156,157,158)
+        idExerciseType["BicepsHard"]    = listOf(159,160,161)
+        idExerciseType["Forearms"]      = listOf(126,127)
+        idExerciseType["Quadriceps"]    = listOf(139,140,141)
+        idExerciseType["CalvesEasy"]    = listOf(162)
+        idExerciseType["CalvesHard"]    = listOf(164)
+        idExerciseType["ABSEasy"]       = listOf(153,154)
+        idExerciseType["ABSHard"]       = listOf(153,154)
+        idExerciseType["Jumping"]       = listOf(148,150,151)
+        idExerciseType["SlowJog"]       = listOf(149)
 
         viewModelScope.launch {
             _loading.value = true
             try {
-                val idExercises : List<Int>
-                val idExerciseStrength1 = listOf(1, 2, 3, 4)
-                val idExerciseStrength2 = listOf(1, 2, 3, 4)
-                val idExerciseStrength3 = listOf(1, 2, 3, 4)
-                val idExercisesCardio1 = listOf(1, 2, 3, 4)
-                val idExercisesCardio2 = listOf(1, 2, 3, 4)
-                val idExercisesCardio3 = listOf(1, 2, 3, 4)
+                val idExercises = mutableMapOf<Int,List<Int>>()
                 val requestBody = mutableMapOf<String, Any>()
                 requestBody["id_user"] = "115dd593-1f58-454f-bd25-318cfd2b4810"
                 requestBody["id_exercise"] = 1
@@ -46,52 +59,114 @@ class VMMakeSchedule : ViewModel() {
                     if (answers[1] == "3-7 times") {
                         requestBody["set"] = 4
                         requestBody["repetition"] = 12
-                        idExercises = idExerciseStrength1
-                    } else if (answers[1] == "1-2 times"){
+                        //Jadwal per hari
+                        idExercises[1] = idExerciseType["ChestEasy"] as List<Int> + idExerciseType["TricepsEasy"] as List<Int>
+                        idExercises[2] = idExerciseType["BackEasy"] as List<Int>
+                        idExercises[3] = idExerciseType["Shoulder"] as List<Int> + idExerciseType["Traps"] as List<Int>
+                        idExercises[5] = idExerciseType["BicepsEasy"] as List<Int> + idExerciseType["Forearms"] as List<Int>
+                        idExercises[6] = idExerciseType["Quadriceps"] as List<Int> + idExerciseType["CalvesEasy"] as List<Int> + idExerciseType["ABSEasy"] as List<Int>
+                    }
+                    else if (answers[1] == "1-2 times"){
                         requestBody["set"] = 4
                         requestBody["repetition"] = 8
-                        idExercises = idExerciseStrength2
-                    } else{
+                        idExercises[1] = idExerciseType["ChestHard"] as List<Int> + idExerciseType["TricepsHard"] as List<Int>
+                        idExercises[2] = idExerciseType["BackHard"] as List<Int>
+                        idExercises[3] = idExerciseType["Shoulder"] as List<Int> + idExerciseType["Traps"] as List<Int>
+                        idExercises[5] = idExerciseType["BicepsHard"] as List<Int> + idExerciseType["Forearms"] as List<Int>
+                        idExercises[6] = idExerciseType["Quadriceps"] as List<Int> + idExerciseType["CalvesHard"] as List<Int> + idExerciseType["ABSHard"] as List<Int>
+                    }
+                    else{
                         requestBody["set"] = 3
                         requestBody["repetition"] = 8
-                        idExercises = idExerciseStrength3
+                        idExercises[1] = idExerciseType["ChestHard"] as List<Int> + idExerciseType["TricepsHard"] as List<Int>
+                        idExercises[2] = idExerciseType["BackHard"] as List<Int>
+                        idExercises[3] = idExerciseType["Shoulder"] as List<Int> + idExerciseType["Traps"] as List<Int>
+                        idExercises[5] = idExerciseType["BicepsHard"] as List<Int> + idExerciseType["Forearms"] as List<Int>
+                        idExercises[6] = idExerciseType["Quadriceps"] as List<Int> + idExerciseType["CalvesHard"] as List<Int> + idExerciseType["ABSHard"] as List<Int>
                     }
+                    insertData(idExercises, requestBody)
                 }
                 else{
+                    // cardio repetition merepresentasikan waktu dalam detik
                     requestBody["type"] = "cardio"
                     if (answers[1] == "3-7 times") {
-                        requestBody["set"] = 4
-                        requestBody["repetition"] = 12
-                        idExercises = idExercisesCardio1
-                    } else if (answers[1] == "1-2 times"){
-                        requestBody["set"] = 4
-                        requestBody["repetition"] = 8
-                        idExercises = idExercisesCardio2
-                    } else{
-                        requestBody["set"] = 3
-                        requestBody["repetition"] = 8
-                        idExercises = idExercisesCardio3
+                        requestBody["set"] = 2
+                        requestBody["repetition"] = 60
+                        idExercises[2] = idExerciseType["Jumping"] as List<Int>
+                        idExercises[6] = idExerciseType["Jumping"] as List<Int>
+                        insertData(idExercises, requestBody)
+
+                        requestBody["set"] = 1
+                        requestBody["repetition"] = 25*60
+                        idExercises.clear()
+                        idExercises[1] = idExerciseType["SlowJog"] as List<Int>
+                        idExercises[5] = idExerciseType["SlowJog"] as List<Int>
+                        insertData(idExercises, requestBody)
+                    }
+                    else if (answers[1] == "1-2 times"){
+                        requestBody["set"] = 2
+                        requestBody["repetition"] = 45
+                        idExercises[2] = idExerciseType["Jumping"] as List<Int>
+                        idExercises[6] = idExerciseType["Jumping"] as List<Int>
+                        insertData(idExercises, requestBody)
+
+                        requestBody["set"] = 1
+                        requestBody["repetition"] = 15*60
+                        idExercises.clear()
+                        idExercises[1] = idExerciseType["SlowJog"] as List<Int>
+                        idExercises[5] = idExerciseType["SlowJog"] as List<Int>
+                        insertData(idExercises, requestBody)
+                    }
+                    else{
+                        requestBody["set"] = 2
+                        requestBody["repetition"] = 30
+                        idExercises[2] = idExerciseType["Jumping"] as List<Int>
+                        idExercises[6] = idExerciseType["Jumping"] as List<Int>
+                        insertData(idExercises, requestBody)
+
+                        requestBody["set"] = 1
+                        requestBody["repetition"] = 10*60
+                        idExercises.clear()
+                        idExercises[1] = idExerciseType["SlowJog"] as List<Int>
+                        idExercises[5] = idExerciseType["SlowJog"] as List<Int>
+                        insertData(idExercises, requestBody)
                     }
                 }
-
-                for (exerciseId in idExercises) {
-                    requestBody["id_exercise"] = exerciseId
-
-                    val apiResponse = ApiService.instance.createSchedule(requestBody)
-
-                    if (apiResponse.isSuccessful) {
-                        val responseString = apiResponse.body()?.string()
-                        Log.d("API Response", "Success for exercise $exerciseId: $responseString")
-                    } else {
-                        val errorMessage = apiResponse.errorBody()?.string() ?: "Unknown error"
-                        Log.e("API Response", "Error for exercise $exerciseId: $errorMessage")
-                    }
-                }
-            } catch (e: Exception) {
+            }
+            catch (e: Exception) {
                 Log.e("Error ethernet", "${e.message}")
                 _error.value = e.message
-            } finally {
+                allSuccess = false
+            }
+            finally {
                 _loading.value = false
+            }
+
+            if (allSuccess) {
+                _success.value = true
+            }
+        }
+    }
+
+    private suspend fun insertData(
+        idExercises: Map<Int, List<Int>>,
+        requestBody: MutableMap<String, Any>
+    ){
+        for((day, exerciseList) in idExercises){
+            requestBody["day"] = day
+            for (exerciseId in exerciseList) {
+                requestBody["id_exercise"] = exerciseId
+
+                val apiResponse = ApiService.instance.createSchedule(requestBody)
+                if (apiResponse.isSuccessful) {
+                    val responseString = apiResponse.body()?.string()
+                    Log.d("API Response", "Success for exercise $exerciseId: $responseString")
+                }
+                else {
+                    val errorMessage = apiResponse.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Response", "Error for exercise $exerciseId: $errorMessage")
+                    throw Exception("failed create schedule: $errorMessage")
+                }
             }
         }
     }
