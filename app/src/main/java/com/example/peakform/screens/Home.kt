ec04687cb684dc8bfe0b29828cbe1ce376b0351a
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2025-06-16 08:07:14 +0700

    feat: update PointStreakInfo to use dynamic user points value

--- app/src/main/java/com/example/peakform/screens/Home.kt
+++ app/src/main/java/com/example/peakform/screens/Home.kt
@@ -116,7 +116,7 @@ fun Home(navController: NavController, userViewModel: VMUser, viewModel: VMHome 
                     )
                 }
 
-                PointStreakInfo(streak = user?.streak ?: 0, points = 1000000, rank = user?.rank ?: 0)
+                PointStreakInfo(streak = user?.streak ?: 0, points = user?.point ?: 0, rank = user?.rank ?: 0)
 
                 if (schedule) {
                     CardImage(
